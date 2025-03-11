21f88c48a46ba8167825f751de01d74634fc692f
squirrel413 <nstibbels@gmail.com>
2025-03-11 09:00:01 -0400

    Upload

--- src/Jukebox.java
+++ src/Jukebox.java
@@ -1,2 +1,15 @@
-public class Jukebox {
+import java.util.*;
+
+public class Jukebox<E> extends HashSet<E>{
+
+    public class Record<K,V>{
+        private K key;
+        private V value;
+
+        public Record(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
 }
--- src/Song.java
+++ src/Song.java
@@ -1,2 +1,28 @@
 public class Song {
+
+    private String name;
+    private String artist;
+    private int timesPlayed;
+
+
+    public Song(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+    }
+
+    public String getName(){return this.name;}
+
+    public String getArtist(){return this.artist;}
+
+    public int getTimesPlayed(){return this.timesPlayed;}
+
+    public String toString(){
+        return name + ", " + artist;
+    }
+
+    public boolean equals(Song other){
+        return this.getName().equals(other.getName()) && this.getArtist().equals(other.getArtist());
+    }
+
+
 }
